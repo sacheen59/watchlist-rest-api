dea4cc8b1200a4153e2b3784f3dc541615efac34
sachin barali <sachinbarali41@gmail.com>
2024-07-18 07:06:23 +0545

    views and serializer

--- watchlist_app/api/serializers.py
+++ watchlist_app/api/serializers.py
@@ -3,7 +3,7 @@ from ..models import WatchList,StreamPlatform,Review
 
 
 class ReviewSerializer(serializers.ModelSerializer):
-
+    review_user = serializers.StringRelatedField(read_only = True)
     class Meta:
         model = Review
         # fields = "__all__"
